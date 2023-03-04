46ad7db8e7027e4e74d4033de908132286d4501c
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-03-04 22:20:28 +0000

    add Index Page

--- Shrimply/Pages/Index.cshtml.cs
+++ Shrimply/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shrimply.Models.Domain;
+using Shrimply.Repositories;
 
 namespace Shrimply.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IShrimpRepository _shrimpRepository;
+        public List<Shrimp> Shrimps { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IShrimpRepository shrimpRepository)
         {
             _logger = logger;
+            _shrimpRepository = shrimpRepository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            Shrimps = (await _shrimpRepository.GetAllAsync()).ToList();
+            return Page();
         }
     }
 }
